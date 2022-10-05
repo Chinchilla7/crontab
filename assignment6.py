6175c4f10228a89b16f97010f9e7a0fb96c7108d
Chinchilla7 <regina.chen@stonybrook.edu>
2022-10-04 23:05:59 -0400

    Update assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -4,19 +4,18 @@ import os
 import sys
 import time
 
+cwd = os.getcwd()
+
+#pulling data from public source
 r = requests.get('https://health.data.ny.gov/resource/d54z-enu8.json')
 r_data = r.json() ## load request as json
 ## load into dataframe
-df = pd.read_json(r_data, orient='index')
+df = pd.DataFrame(r_data)
 
-# get current working directory
-cwd = os.getcwd()
-
-# print cwd
-print(cwd)
+df.to_csv('./blood_lead_testing.csv')
 
 # create a new dictionary with dummy data
-data = {'a': 1, 'b': 2, 'c': 3}
+data = r_data
 
 # get the current time
 now = time.time()
@@ -25,7 +24,6 @@ now = time.time()
 nowStr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
 
 # create a new file in the current working directory
-with open(cwd + '/testFile_' + nowStr + '.txt', 'w') as f:
+with open(cwd + '/assignment5_' + nowStr + '.txt', 'w') as f:
     f.write(str(data))
 
-# /Library/Frameworks/Python.framework/Versions/3.10/bin/python3
